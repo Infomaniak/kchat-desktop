5c37c1c5afc013e5be4c1fc9003c5fd3b6d101b7
Glauda Florent <florent.glauda@infomaniak.com>
2022-06-13 15:13:36 +0200

    fix for installer

--- src/assets/linux/create_desktop_file.sh
+++ src/assets/linux/create_desktop_file.sh
@@ -5,14 +5,14 @@ THIS_PATH=`readlink -f $0`
 cd `dirname ${THIS_PATH}`
 FULL_PATH=`pwd`
 cd ${WORKING_DIR}
-cat <<EOS > Mattermost.desktop
+cat <<EOS > kChat.desktop
 [Desktop Entry]
 Name=Mattermost
-Comment=Mattermost Desktop application for Linux
+Comment=kChat Desktop application for Linux
 Exec="${FULL_PATH}/mattermost-desktop"
 Terminal=false
 Type=Application
 Icon=${FULL_PATH}/app_icon.png
 Categories=Network;InstantMessaging;
 EOS
-chmod +x Mattermost.desktop
+chmod +x kChat.desktop
