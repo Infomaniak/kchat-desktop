798698d97bf8d1d40d6e7a338a4a953b5ee54bf4
anton.buksa <anton.buksa@infomaniak.com>
2022-09-27 13:30:29 +0200

    add app download links to gitlab releases
    Changelog: added

--- release.sh
+++ release.sh
@@ -13,6 +13,9 @@ SLACK_NOTIFY_CHANNEL=$5
 postchangelog=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "version=${GITLAB_PROJECT_TAG}" "https://gitlab.infomaniak.ch/api/v4/projects/3236/repository/changelog")
 changelog=$(curl --request GET --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "version=${GITLAB_PROJECT_TAG}" "https://gitlab.infomaniak.ch/api/v4/projects/3236/repository/changelog" | jq -r '.notes')
 release=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "name=${GITLAB_PROJECT_TAG}&tag_name=${GITLAB_PROJECT_TAG}&description=${changelog}&milestones=${MILESTONE}" "https://gitlab.infomaniak.ch/api/v4/projects/3236/releases")
+release=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "name=Mac-x64.dmg&url=https://download.storage5.infomaniak.com/kchat/kchat-desktop-${GITLAB_PROJECT_TAG}-mac-x64.dmg" "https://gitlab.infomaniak.ch/api/v4/projects/3236/releases/${GITLAB_PROJECT_TAG}/assets/links")
+release=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "name=Linux-x86_64.AppImage&url=https://download.storage5.infomaniak.com/kchat/kchat-desktop-${GITLAB_PROJECT_TAG}-linux-x86_64.AppImage" "https://gitlab.infomaniak.ch/api/v4/projects/3236/releases/${GITLAB_PROJECT_TAG}/assets/links")
+release=$(curl --write-out '%{http_code}' --request POST --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_ACCESS_TOKEN}" --data "name=Windows-x86_64.exe&url=https://download.storage5.infomaniak.com/kchat/kchat-desktop-setup-${GITLAB_PROJECT_TAG}-win.exe" "https://gitlab.infomaniak.ch/api/v4/projects/3236/releases/${GITLAB_PROJECT_TAG}/assets/links")
 
 commit_url="https://gitlab.infomaniak.ch/kchat/desktop/-/commit/"
 mr_url="https://gitlab.infomaniak.ch/kchat/desktop/-/merge_requests/"
