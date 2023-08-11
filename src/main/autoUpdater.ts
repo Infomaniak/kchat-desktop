d5dbbee0ea85d46089bea552a11f92c7e00b7103
Benoit Nau <benoit.nau@infomaniak.com>
2023-08-11 09:36:58 +0200

    fix: view
    Auto-updater modal displays a blurred icon.
    
    Changelog: fixed

--- src/main/autoUpdater.ts
+++ src/main/autoUpdater.ts
@@ -36,7 +36,7 @@ autoUpdater.autoDownload = false;
 autoUpdater.disableWebInstaller = true;
 
 const assetsDir = path.resolve(app.getAppPath(), 'assets');
-const appIconURL = path.resolve(assetsDir, 'appicon_with_spacing_32.png');
+const appIconURL = path.resolve(assetsDir, 'appicon_with_spacing_96.png');
 const appIcon = nativeImage.createFromPath(appIconURL);
 
 /** to test this during development
