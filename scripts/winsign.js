13e9538a86929b96f012726e073a85645d99352b
anton.buksa <anton.buksa@infomaniak.com>
2024-06-28 14:38:49 +0200

    try another codesign script for win

--- scripts/winsign.js
+++ scripts/winsign.js
@@ -2,20 +2,65 @@
 const {execSync} = require('child_process');
 
 exports.default = async (config) => {
-    const keypairAlias = process.env.SM_KEYPAIR_ALIAS;
-    const path = config.path ? String(config.path) : '';
+    // if (!process.env.SM_INSTALL_DIR) {
+    //     throw new Error('Unable to sign files because the path to smctl.exe is not set in the environment.');
+    // }
 
-    if (!keypairAlias || !path) {
-        throw new Error('Signing failed: missing vars');
+    // if (!process.env.SIGNTOOL_DIR) {
+    //     throw new Error('Unable to sign files because the path to signtool.exe is not set in the environment.');
+    // }
+
+    // Common
+    const filePath = `"${config.path.replace(/\\/g, '/')}"`;
+    const smctlDir = 'C:\\Program Files\\DigiCert\\DigiCert One Signing Manager Tools'; //`"${process.env.SM_INSTALL_DIR}"`;
+    const signToolDir = 'C:\\Program Files (x86)\\Windows Kits\\10\\App Certification Kit'; //`"${process.env.SIGNTOOL_DIR}"`;
+
+    try {
+        const signCommand = '.\\scripts\\sign.ps1';
+        const keyPairAlias = `"${process.env.SM_KEYPAIR_ALIAS}"`;
+        const sign = [
+            'pwsh',
+            '-NoProfile',
+            '-ExecutionPolicy Unrestricted',
+            `-Command \"$Input | ${signCommand}`,
+            `-FilePath '${filePath}'`,
+            `-KeyPairAlias '${keyPairAlias}'`,
+            `-SmctlDir '${smctlDir}'`,
+            `-SignToolDir '${signToolDir}'\"`,
+        ];
+        const signStdout = execSync(sign.join(' ')).toString();
+        if (signStdout.match(/FAILED/)) {
+            console.error(
+                `Error detected in ${signCommand}: [${signStdout}]`,
+            );
+            throw new Error(`Error detected in ${signCommand}: [${signStdout}]`);
+        }
+    } catch (e) {
+        throw new Error(`Exception thrown during code signing: ${e.message}`);
     }
 
-    const output = execSync(
-        `smctl sign --keypair-alias=${keypairAlias} --input="${path}" --verbose`,
-    ).
-        toString().
-        trim();
-
-    if (!output.includes('Done Adding Additional Store')) {
-        throw new Error(`Failed to sign executable: ${output}`);
+    // Verify the signature
+    try {
+        const verifyCommand = '.\\scripts\\verify.ps1';
+        const fingerprint = `"${process.env.SM_CODE_SIGNING_CERT_SHA1_HASH}"`;
+        const verify = [
+            'pwsh',
+            '-NoProfile',
+            '-ExecutionPolicy Unrestricted',
+            `-Command \"$Input | ${verifyCommand}`,
+            `-FilePath '${filePath}'`,
+            `-Fingerprint '${fingerprint}'`,
+            `-SmctlDir '${smctlDir}'`,
+            `-SignToolDir '${signToolDir}'\"`,
+        ];
+        const verifyStdout = execSync(verify.join(' ')).toString();
+        if (verifyStdout.match(/FAILED/)) {
+            console.error(
+                `Error detected in ${verifyCommand}: [${verifyStdout}]`,
+            );
+            throw new Error(`Error detected in ${verifyCommand}: [${verifyStdout}]`);
+        }
+    } catch (e) {
+        throw new Error(`Exception thrown during signature verification: ${e.message}`);
     }
 };
