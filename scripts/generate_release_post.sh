8092a2fde60741b89f95b3ab27915c3a8ebc4e4b
Boris Trombert <boris.trombert@infomaniak.com>
2026-05-01 17:16:00 +0200

    fix: add arm package to ci instead of npm rebuild

--- scripts/generate_release_post.sh
+++ scripts/generate_release_post.sh
@@ -4,7 +4,8 @@ set -eu
 VERSION="${VERSION:-}"
 RUN_URL="${RUN_URL:-}"
 BUILD_LINUX_RESULT="${BUILD_LINUX_RESULT:-}"
-BUILD_MSI_RESULT="${BUILD_MSI_RESULT:-}"
+BUILD_LINUX_ARM_RESULT="${BUILD_LINUX_ARM_RESULT:-}"
+BUILD_WINDOWS_RESULT="${BUILD_WINDOWS_RESULT:-}"
 GITHUB_RELEASE_RESULT="${GITHUB_RELEASE_RESULT:-}"
 
 failed_jobs=()
@@ -12,8 +13,11 @@ failed_jobs=()
 if [ "$BUILD_LINUX_RESULT" != "success" ] && [ -n "$BUILD_LINUX_RESULT" ]; then
     failed_jobs+=("build-linux: \`$BUILD_LINUX_RESULT\`")
 fi
-if [ "$BUILD_MSI_RESULT" != "success" ] && [ -n "$BUILD_MSI_RESULT" ]; then
-    failed_jobs+=("build-msi-installer: \`$BUILD_MSI_RESULT\`")
+if [ "$BUILD_LINUX_ARM_RESULT" != "success" ] && [ -n "$BUILD_LINUX_ARM_RESULT" ]; then
+    failed_jobs+=("build-linux-arm: \`$BUILD_LINUX_ARM_RESULT\`")
+fi
+if [ "$BUILD_WINDOWS_RESULT" != "success" ] && [ -n "$BUILD_WINDOWS_RESULT" ]; then
+    failed_jobs+=("build-windows: \`$BUILD_WINDOWS_RESULT\`")
 fi
 if [ "$GITHUB_RELEASE_RESULT" != "success" ] && [ -n "$GITHUB_RELEASE_RESULT" ]; then
     failed_jobs+=("github-release: \`$GITHUB_RELEASE_RESULT\`")
