1aa1f878c7ca28f7e2f4ecb47dfc9e43838b728f
Boris Trombert <boris.trombert@infomaniak.com>
2026-05-06 10:21:35 +0200

    fix(settings): remove legacy Bootstrap CSS causing modal backdrop misalignment

--- src/renderer/modals/settings/settings.tsx
+++ src/renderer/modals/settings/settings.tsx
@@ -1,9 +1,7 @@
 // Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'renderer/css/index.css';
-import 'renderer/css/settings.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
