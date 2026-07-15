92f3ec395dff28eedd0da8888d6fe5910d326213
Aurélien Sberro <aurelien.sberro@infomaniak.com>
2026-07-15 11:55:12 +0200

    fix(ci): Use auto-detected release channel and explicit publish in CI

--- scripts/Makefile.ps1
+++ scripts/Makefile.ps1
@@ -530,7 +530,7 @@ function Run-BuildElectron {
     #npm run build --prefix="$(Get-RootDir)" "$(Get-RootDir)"
     Print-Info "Packaging nodejs/electron for Windows (running npm run package:windows)..."
     # NSIS has the upgrade flag enabled, so it must be done first
-    npm run package:windows-nsis
+    npm run package:windows-nsis -- --publish always
     # npm run package:windows
     #npm run package:windows --prefix="$(Get-RootDir)" "$(Get-RootDir)"
 }
